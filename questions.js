f8d04ee9869438f9ad93eb40a20de8e0f523fdda
Saurabh Dayal Solanki <134385036+Saurabh-dayal@users.noreply.github.com>
2023-12-09 14:16:22 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        num: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        num: 2,
+        question: "Who is considered the father of Computers?",
+        answer: "B. Charles Babbage",
+        options: [
+            "A. James Gosling",
+            "B. Charles Babbage",
+            "C. Dennis Ritchie",
+            "D. Bjarne Stroustrup"
+        ]
+    },
+    {
+        num: 3,
+        question: "Which of the following is the correct definition of a Computer?",
+        answer: "A. Computer is a machine or device that can be programmed to perform arithmetical or logic operation sequences automatically",
+        options: [
+            "A. Computer is a machine or device that can be programmed to perform arithmetical or logic operation sequences automatically",
+            "B. Computer understands only binary language which is written in the form of 0s & 1s",
+            "C. Computer is a programmable electronic device that stores, retrieves, and processes data",
+            "D. All of the mentioned"
+        ]
+    },
+    {
+        num: 4,
+        question: "What is the full form of CPU?",
+        answer: "C. Central Processing Unit",
+        options: [
+            "A. Computer Processing Unit",
+            "B. Computer Principle Unit",
+            "C. Central Processing Unit",
+            "D. Control Processing Unit"
+        ]
+    },
+    {
+        num: 5,
+        question: "Which of the following computer languages is written in binary codes only?",
+        answer: "B. machine language",
+        options: [
+            "A. Pascal",
+            "B. machine language",
+            "C. C",
+            "D. C#"
+        ]
+    }
+];
